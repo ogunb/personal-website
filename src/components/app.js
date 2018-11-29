3fa2f5d605af7854241ded340ac5789a7fe41979
ogunb <babacanogun@gmail.com>
2018-11-29 17:18:13 +0300

    remove preact-router.

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,12 @@
 import { h, Component } from 'preact';
-import { Router } from 'preact-router';
 
 import Hero from './Hero';
 
 class App extends Component {
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
-
 	render() {
 		return (
 			<div id="app">
-				<Router onChange={this.handleRoute}>
-					<Hero path="/" />
-				</Router>
+				<Hero />
 			</div>
 		);
 	}
