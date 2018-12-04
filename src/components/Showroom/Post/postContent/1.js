636d21a58584cfa1532f764ace74bcae9fd587c7
ogunb <babacanogun@gmail.com>
2018-12-05 00:56:59 +0300

    add rennie and sleepy posts.

--- src/components/Showroom/Post/postContent/1.js
+++ src/components/Showroom/Post/postContent/1.js
@@ -1,3 +1,14 @@
-const sleepy = 'asdasıdhfuasd';
+const sleepy = `![sleepy](https://raw.githubusercontent.com/ogunb/sleepy/master/sleepy.jpg "Sleepy")
+
+## **sleepy💤**
+
+sleepy is a sleep-cycle calculator.
+[Live Demo](https://ogunb.github.io/sleepy/dist/)
+
+### **What is a sleep cycle?**
+
+For an average human a sleep cycle lasts about 90 minutes and during that time we move through five stages of sleep. You should wake up between the cycles for a well done sleep.
+
+With this app you can calculate the time you should go to bed, or the time you should wake up.`;
 
 export default sleepy;
