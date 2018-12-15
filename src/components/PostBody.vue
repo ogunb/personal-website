fb5c18c3178a302e70803fa789f55a8f476b799d
ogunb <babacanogun@gmail.com>
2018-12-16 00:06:29 +0300

    add postbody

--- src/components/PostBody.vue
+++ src/components/PostBody.vue
@@ -1,9 +1,24 @@
-<template></template>
+<template>
+  <div class="showroom__post__body" v-if="isOpen">
+    <VueSimpleMarkdown></VueSimpleMarkdown>
+  </div>
+</template>
 
 <script>
+import VueSimpleMarkdown from "vue-simple-markdown";
+import "vue-simple-markdown/dist/vue-simple-markdown.css";
+
 export default {
   name: "post-body",
-  props: ["isOpen"]
+  props: ["isOpen"],
+  data() {
+    return {
+      input: ""
+    };
+  }
+  // created: function() {
+  //   return this.input
+  // },
 };
 </script>
 
